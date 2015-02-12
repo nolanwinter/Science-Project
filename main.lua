79ee003cd3751bd2a8a66c34fba84e12f76e1094
nolanwinter <winter.drummer.guy@gmail.com>
2015-02-12 09:24:44 -0600

    Getting closer....

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
-local car = display.newImage( "Car.png" )
-car.x = 512; car.y = 384
-car.width = 1024; car.height = 768
+local street = display.newImage( "Street.png" )
+street.x = 512; street.y = 184
+street.width = 1024; street.height = 768
+local foreground = display.newImage( "Car.png" )
+foreground.x = 512; foreground.y = 384
+foreground.width = 1024; foreground.height = 768
+local brake = display.newImage( "Pedal.png" )
+brake.x = 360; brake.y = 584
+local gas = display.newImage( "Pedal.png" )
+gas.x = 660; gas.y = 584
+local car = display.newImage( "FullCar.png" )
