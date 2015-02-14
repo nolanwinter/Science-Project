ad94c79bd58d048d72a601b3b76c9bafc9752f49
nolanwinter <winter.drummer.guy@gmail.com>
2015-02-14 17:14:51 -0600

    Getting farther

--- main.lua
+++ main.lua
@@ -1,3 +1,61 @@
 timeTester = require( "TimeTester" )
 showResults = require( "ShowResults" )
+
+local sequence = {
+  {
+    {duration = 2, isCar = true},
+    {duration = 7, isCar = false},
+    {duration = 4, isCar = true},
+    {duration = 6, isCar = true},
+    {duration = 3, isCar = false},
+    {duration = 3, isCar = false},
+    {duration = 7, isCar = true},
+    {duration = 5, isCar = true},
+    {duration = 4, isCar = false},
+    {duration = 3, isCar = true}
+    {
+  },
+  {
+    {duration = 3, isCar = false},
+    {duration = 5, isCar = true},
+    {duration = 2, isCar = true},
+    {duration = 6, isCar = false},
+    {duration = 3, isCar = true},
+    {duration = 2, isCar = false},
+    {duration = 7, isCar = true},
+    {duration = 6, isCar = true},
+    {duration = 4, isCar = true},
+    {duration = 3, isCar = false}
+  },
+  {
+    {duration = 6, isCar = true},
+    {duration = 4, isCar = true},
+    {duration = 9, isCar = false},
+    {duration = 4, isCar = true},
+    {duration = 3, isCar = false},
+    {duration = 8, isCar = true},
+    {duration = 4, isCar = false},
+    {duration = 8, isCar = false},
+    {duration = 3, isCar = true},
+    {duration = 2, isCar = true}
+  },
+  {
+    {duration = 4, isCar = true},
+    {duration = 6, isCar = false},
+    {duration = 8, isCar = true},
+    {duration = 5, isCar = true},
+    {duration = 3, isCar = false},
+    {duration = 6, isCar = false},
+    {duration = 7, isCar = false},
+    {duration = 2, isCar = true},
+    {duration = 8, isCar = true},
+    {duration = 2, isCar = true}
+  }
+}
+  
+for i = 1,4 do
+  print( sequence[2][i].duration )
+  print( sequence[2][i].isCar )
+end
+
 timeTester.open()
